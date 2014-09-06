e4863f2aea6ec8fae9e86c1ce47f0120979d3b4c
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2014-09-06 11:25:28 +0200

    Square brackets and different structure for messages

--- src/config/config.php
+++ src/config/config.php
@@ -1,12 +1,12 @@
 <?php
 
-return array(
+return [
 
-	'enabled'       => true,
+	'enabled' => true,
 
 	// Hipchat settings
-	'room'          => null,
-	'color'         => null,
+	'room'    => null,
+	'color'   => null,
 
 	// Message
 	// You can use the following variables :
@@ -14,7 +14,8 @@ return array(
 	// 2: Branch
 	// 3: Connection and stage
 	// 4: Host
-	'before_deploy' => '{1} is deploying "{2}" on "{3}" ({4})',
-	'after_deploy'  => '{1} finished deploying branch "{2}" on "{3}" ({4})',
-
-);
+	'messages' => [
+		'before_deploy' => '{1} is deploying "{2}" on "{3}" ({4})',
+		'after_deploy'  => '{1} finished deploying branch "{2}" on "{3}" ({4})',
+	],
+];
